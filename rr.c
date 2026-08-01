f1df5a0fceeccc43c83d134045be42a79a7aa53d
Qapky <fauzanmulky3@gmail.com>
2026-08-01 07:08:40 +0700

    calculatre rr times

--- rr.c
+++ rr.c
@@ -77,7 +77,7 @@ void execute_process_quantum(Process *p, int quantum) {
         CloseHandle(pi.hProcess);
         CloseHandle(pi.hThread);
     } else {
-        cpu_intensive_work(quantum);   /* fallback */
+        cpu_intensive_work(quantum);
     }
 #else
     pid_t child = fork();
@@ -91,7 +91,79 @@ void execute_process_quantum(Process *p, int quantum) {
 #endif
 }
 
-void calculate_rr_times()
-{
+void calculate_rr_times(Process p[], int n, int quantum) {
+    double current_time = 0;
+    int    completed    = 0;
 
+    for (int i = 0; i < n; i++) {
+        p[i].remaining_time = p[i].burst_time;
+        p[i].first_response = 1;
+        p[i].calc_start_time = -1;
+    }
+
+    int queue[200], front = 0, rear = 0;
+    int in_queue[200];
+    for (int i = 0; i < n; i++) in_queue[i] = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (p[i].arrival_time == 0) {
+            queue[rear++] = i;
+            in_queue[i]   = 1;
+        }
+    }
+
+    while (completed < n) {
+        if (front == rear) {
+            double next_arrival = 999999;
+            int    next_idx     = -1;
+            for (int i = 0; i < n; i++) {
+                if (p[i].remaining_time > 0 && p[i].arrival_time > current_time) {
+                    if (p[i].arrival_time < next_arrival) {
+                        next_arrival = p[i].arrival_time;
+                        next_idx     = i;
+                    }
+                }
+            }
+            if (next_idx != -1) {
+                current_time    = next_arrival;
+                queue[rear++]   = next_idx;
+                in_queue[next_idx] = 1;
+            }
+        } else {
+            int idx = queue[front++];
+
+            if (p[idx].first_response) {
+                p[idx].calc_start_time    = current_time;
+                p[idx].calc_response_time = current_time - p[idx].arrival_time;
+                p[idx].first_response     = 0;
+            }
+
+            int exec_time = (p[idx].remaining_time < quantum)
+                            ? p[idx].remaining_time : quantum;
+            p[idx].remaining_time -= exec_time;
+            current_time          += exec_time;
+
+            for (int i = 0; i < n; i++) {
+                if (!in_queue[i] && p[i].arrival_time <= current_time
+                    && p[i].remaining_time > 0) {
+                    queue[rear++] = i;
+                    in_queue[i]   = 1;
+                }
+            }
+
+            if (p[idx].remaining_time > 0) {
+                queue[rear++] = idx;
+            } else {
+                p[idx].calc_completion_time  = current_time;
+                p[idx].calc_turnaround_time  = p[idx].calc_completion_time
+                                               - p[idx].arrival_time;
+                p[idx].calc_waiting_time     = p[idx].calc_turnaround_time
+                                               - p[idx].burst_time;
+                completed++;
+                in_queue[idx] = 0;
+            }
+        }
+    }
 }
+
+
